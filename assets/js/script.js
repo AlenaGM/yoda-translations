a1399a7c7fb93138456f6b09c083f5d8271bd664
AlenaGM <alena.guillaume4@gmail.com>
2023-12-15 11:56:16 +0100

    upd design test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+document.addEventListener('mousemove', e => {
+	Object.assign(document.documentElement, {
+		style: `
+		--move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
+		--move-y: ${(e.clientY - window.innerHeight / 2) * .01}deg;
+		`
+	})
+})
+
 document.querySelector("#submit").addEventListener("click", function () {
   let text = document
     .querySelector("#sourceText")
